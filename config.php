7bcca1a63c0e60d6c21b43dbd51b66ff4fefbb30
João Augusto <j.asoul@hotmail.com>
2023-07-03 02:55:23 -0300

    ajustes

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
 //if($conexao ->connect_errno){
 
-    echo "Erro";
+    //echo "Erro";
 ////}
 //else{
  //   echo "Conexao efeutada com sucesso!";
